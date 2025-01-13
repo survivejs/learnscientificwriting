c05bb54bc3e93145960631f36264f7889b680982
Juho Vepsäläinen <bebraw@gmail.com>
2025-01-13 11:12:42 +0200

    feat: Add basic navigation

--- site/globalUtilities.ts
+++ site/globalUtilities.ts
@@ -1,9 +1,16 @@
-import { urlJoin } from "https://bundle.deno.dev/https://deno.land/x/url_join@1.0.0/mod.ts";
+import { urlJoin as urlJoinFn } from "https://bundle.deno.dev/https://deno.land/x/url_join@1.0.0/mod.ts";
 import type { DataSourcesApi } from "https://deno.land/x/gustwind@v0.77.2/types.ts";
 
 function init(o: DataSourcesApi) {
-  // Add your global page utilities here.
-  // Alternatively they can be defined per component.
+  function urlJoin(...parts: string[]) {
+    if (!parts.every((s) => typeof s === "string")) {
+      console.error(parts);
+      throw new Error("Failed to join url");
+    }
+
+    return urlJoinFn(...parts);
+  }
+
   return { urlJoin };
 }
 
