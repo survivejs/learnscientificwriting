887f06bb8768267b07b897f8fec3def93e47b3d9
Juho Vepsäläinen <bebraw@gmail.com>
2026-04-30 12:17:32 +0300

    Upgrade Gustwind to 0.100.0

--- site/components/Markdown.server.ts
+++ site/components/Markdown.server.ts
@@ -1,12 +1,13 @@
 import getMarkdown from "../transforms/markdown.ts";
 import type { DataSourcesApi } from "gustwind";
+import { unwrapRawHtml } from "gustwind/htmlisp";
 
 function init({ load }: DataSourcesApi) {
   const markdown = getMarkdown(load);
 
   return {
-    processMarkdown: async (input: string | { value: string }) =>
-      (await markdown(typeof input === "string" ? input : input.value)).content,
+    processMarkdown: async (input: unknown) =>
+      (await markdown(unwrapRawHtml(input))).content,
   };
 }
 
