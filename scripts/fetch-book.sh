377b7dda58e0278739e2e687e4e85f52ed67c4bf
Juho Vepsäläinen <bebraw@gmail.com>
2026-04-30 14:03:32 +0300

    Bootstrap book for Worker builds

--- scripts/fetch-book.sh
+++ scripts/fetch-book.sh
@@ -2,4 +2,10 @@
 
 set -e # Exit with nonzero exit code if anything fails
 
-git clone -b main "https://github.com/survivejs/scientificwritingbook.git" "book/"
+if [ -d "book/.git" ]; then
+  git -C "book" fetch origin main
+  git -C "book" checkout main
+  git -C "book" pull --ff-only origin main
+else
+  git clone -b main "https://github.com/survivejs/scientificwritingbook.git" "book/"
+fi
